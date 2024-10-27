ce2c156846abdf3475ad56d977a743747431e8bb
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2024-10-27 13:14:47 +0100

    resolve first task called poligon

--- 01_helloapi/01.ts
+++
@@ -1,8 +0,0 @@
-import { get_token_and_task_data, send_answer } from "../modules/tasks"
-
-const helloTask = async () => {
-    const data = await get_token_and_task_data("helloapi")
-    await send_answer(data.cookie)
-}
-
-helloTask()
